8d1dc69f0a8e0600e9cc4c9d55d332e39927eb38
Kyle Ringler <92175614+linuxpy76@users.noreply.github.com>
2024-05-13 19:24:43 -0500

    Added system backup functionality (#6)
    * Added system backup fuctionality
    
    * Added comments

--- mikrotikconfigbackup.py
+++ mikrotikconfigbackup.py
@@ -19,9 +19,10 @@ parser = argparse.ArgumentParser(description="Logs into MikroTik router and crea
 # Add arguments
 parser.add_argument("-r", "--router-ip", type=str, help="Hostname or remote IP address without CIDR notation. \nEx. 192.168.10.1 or router.org.lan")
 parser.add_argument("-u", "--username", type=str, help="Username Ex. admin")
-parser.add_argument("-p", "--password", type=str, help="Enter your password. If you skip this option then you will be prompted for it. (hides the pass)")
+parser.add_argument("-p", "--password", type=str, help="Enter router password. If you skip this option then you will be prompted for it. (hides the pass)")
 parser.add_argument("-d", "--directory", type=str, help="Path to save config file. If backup is not defined then the remote copy and delete operations will be skipped. \nEx. C:\\backup\\")
 parser.add_argument("--delete", action="store_true", help="Delete the remote backup file from the router. Default: False")
+parser.add_argument("-e", "--encryption-password", type=str, help="Enter your backup encryption password.")
 
 # Parse the command-line arguments
 args = parser.parse_args()
@@ -37,6 +38,7 @@ username = args.username
 password = args.password
 directory = args.directory
 delete_remote_file = args.delete
+encryption_password = args.encryption_password
 
 # If arguments aren't supplied then ask user for input to define SSH parameters
 if args.router_ip is None:
@@ -51,6 +53,9 @@ if args.password is None:
 if args.directory is None:
     # Prompt for Directory
     directory = input("Enter the local backup directory: ").strip()
+if args.encryption_password is None:
+    # Prompt for Encryption Password
+    encryption_password = getpass.getpass(prompt="Encryption Password: ").strip()
 
 # Define function to append endswitch to path if there isn't one
 def save_backslash_to_path(path):
@@ -79,45 +84,83 @@ try:
 
     # Get current date and time for backup file naming
     now = datetime.datetime.now()
-    backup_filename = f"{router_identity}_backup_{now.strftime('%Y-%m-%d-%H-%M-%S')}.backup"
-    ext_filename = f"{backup_filename}.rsc"
+    cfg_bak_filename = f"{router_identity}_cfg_backup_{now.strftime('%Y-%m-%d-%H%M%S')}"
+    # cfg_bak_filename = f"{cfg_bak_filename}.rsc"
+    sys_bak_filename = f"{router_identity}_sys_backup_{now.strftime('%Y-%m-%d-%H%M%S')}"
+    # sys_bak_filename = f"{sys_bak_filename}.backup"
 
-    # Execute the command
-    command = f"/export file={backup_filename}"
-    stdin, stdout, stderr = ssh.exec_command(command)
-
+    # Execute the config backup command
+    cfg_bak_cmd = f"/export file={cfg_bak_filename}"
+    stdin, stdout, stderr = ssh.exec_command(cfg_bak_cmd)
     # Wait for the command to complete
     stdout.channel.recv_exit_status()
     
+    # Rename config backup for file checks
+    cfg_bak_filename = f"{cfg_bak_filename}.rsc"
+
+    # Execute the system backup command
+    if "encryption_password" in globals() and encryption_password != "":
+        print("System backup will be encrypted with aes-sha256 and provided password.")
+        sys_bak_cmd = f"/system backup save name={sys_bak_filename} password={encryption_password}"
+        stdin, stdout, stderr = ssh.exec_command(sys_bak_cmd)
+        # Wait for the command to complete
+        stdout.channel.recv_exit_status()
+        output = stdout.read().decode('utf-8')
+    else:
+        print("WARNING! System backup will not be encrypted. No password provided.")
+        sys_bak_cmd = f"/system backup save name={sys_bak_filename} dont-encrypt yes"
+        stdin, stdout, stderr = ssh.exec_command(sys_bak_cmd)
+        # Wait for the command to complete
+        stdout.channel.recv_exit_status()
+        output = stdout.read().decode('utf-8')
+
+    # Rename System backup for file checks
+    sys_bak_filename = f"{sys_bak_filename}.backup"
+
     # Check if the backup file was created successfully
     sftp_client = ssh.open_sftp()
     remote_files = sftp_client.listdir()
 
-    # Check that the file was created.
-    if ext_filename in remote_files:
-        file_size = sftp_client.stat(ext_filename).st_size
-        if file_size > 0:
-            print(f"Backup completed successfully. Configuration saved as '{ext_filename}'.")
+    # Check that the config backup file was created
+    if cfg_bak_filename in remote_files:
+        cfg_bak_filesize = sftp_client.stat(cfg_bak_filename).st_size
+        if cfg_bak_filesize > 0:
+            print(f"Config backup completed successfully. Configuration saved as '{cfg_bak_filename}'.")
         else:
-            print("Backup file size is 0. Backup may have failed.")
+            print("Config backup file size is 0. Backup may have failed.")
     else:
-        print("Backup file not found. Backup may have failed.")
+        print("Config backup file not found. Backup may have failed.")
+    
+    # Check that the system backup file was created
+    if sys_bak_filename in remote_files:
+        sys_bak_filesize = sftp_client.stat(sys_bak_filename).st_size
+        if sys_bak_filesize > 0:
+            print(f"System backup completed successfully. Configuration saved as '{sys_bak_filename}'.")
+        else:
+            print("System backup file size is 0. Backup may have failed.")
+    else:
+        print("System backup file not found. Backup may have failed.")
 
-    # Backup file to local directory
-    if ext_filename in remote_files and directory:
-        # Define local path for the backup file
-        local_backup_path = directory + ext_filename
+    # Backup files to local directory
+    if cfg_bak_filename in remote_files and directory:
+        # Define local path for the config backup file
+        cfg_bak_path = directory + cfg_bak_filename
+        sys_bak_path = directory + sys_bak_filename
 
         # Download the backup file to the local directory
-        sftp_client.get(ext_filename, local_backup_path)
-        print(f"Backup copied to '{local_backup_path}'")
+        sftp_client.get(cfg_bak_filename, cfg_bak_path)
+        print(f"Config backup copied to '{cfg_bak_path}'")
+        sftp_client.get(sys_bak_filename, sys_bak_path)
+        print(f"System backup copied to '{sys_bak_path}'")
 
         # Remove the backup file from the router (optional)
         if delete_remote_file == True:
-            sftp_client.remove(ext_filename)
-            print(f"Backup file '{ext_filename}' removed from the router.")
+            sftp_client.remove(cfg_bak_filename)
+            print(f"Config backup file '{cfg_bak_filename}' removed from the router.")
+            sftp_client.remove(sys_bak_filename)
+            print(f"System backup file '{sys_bak_filename}' removed from the router.")
     
-    elif ext_filename in remote_files and not directory:
+    elif cfg_bak_filename in remote_files and not directory:
         print("Local backup directory not provided. Skipping file transfer and deletion.")
  
     else:
